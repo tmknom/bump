ed159c3973dcc7c410f30b07caa39cbc3a9b6f0c
tmknom <tmk.nom@gmail.com>
2021-10-24 17:23:30 +0900

    feat: add support for specifying the version from command line args

--- cli.go
+++ cli.go
@@ -7,18 +7,24 @@ const InitialVersion = "0.1.0"
 
 func Handle() error {
 	flag.Parse()
+
+	var argVersion string
+	if flag.NArg() > 1 {
+		argVersion = flag.Arg(1)
+	}
+
 	switch flag.Arg(0) {
 	case "init":
 		cmd := &InitCommand{}
 		return cmd.Run(InitialVersion, VersionFile)
 	case "major":
-		cmd := &MajorCommand{}
+		cmd := &MajorCommand{version: argVersion}
 		return cmd.Run(VersionFile)
 	case "minor":
-		cmd := &MinorCommand{}
+		cmd := &MinorCommand{version: argVersion}
 		return cmd.Run(VersionFile)
 	case "patch":
-		cmd := &PatchCommand{}
+		cmd := &PatchCommand{version: argVersion}
 		return cmd.Run(VersionFile)
 	case "show":
 		cmd := &ShowCommand{}
